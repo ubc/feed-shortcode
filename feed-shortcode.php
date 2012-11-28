aa8592b3ebc7ec1f4983600b622aebe54bcc419a
Michael Ha <mha.hk.ca@gmail.com>
2012-11-28 15:01:52 -0800

    added skeleton

---
+++ feed-shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Plugin Name: Feed Shortcode
+* Plugin URI:
+* Description: A [feed] shortcode plugin.
+* Version: 0.1
+* Author: UBC CMS
+* Author URI:
+*/
+
+class CTLT_Feed_Shortcode {
+
+}
+
+new CTLT_Feed_Shortcode();
