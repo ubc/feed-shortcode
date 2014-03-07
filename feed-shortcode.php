69eb804aeb6b0cf4c9bfbed04d0bc31ec1817443
Enej Bajgoric <enej.bajgoric@gmail.com>
2014-03-07 12:31:37 -0800

    Adding the next and previous month links back in

--- feed-shortcode.php
+++ feed-shortcode.php
@@ -613,11 +613,12 @@ class CTLT_Feed_Shortcode {
 				endforeach;
 				?>
 				</table> 
-				
+				<?php if( $current < 20 && $current > -20 ) { ?>
 				<p>
-				<a href="?current=<?php echo $previous_month; ?>" class="button btn"><i class="icon-chevron-left"></i> Previous Month</a>
-				<a href="?current=<?php echo $next_month; ?>" class="button btn">Next Month <i class="icon-chevron-right"></i></a>
+				<a href="?current=<?php echo $previous_month; ?>" rel="nofollow" class="button btn"><i class="icon-chevron-left"></i> Previous Month</a>
+				<a href="?current=<?php echo $next_month; ?>" rel="nofollow" class="button btn">Next Month <i class="icon-chevron-right"></i></a>
 				</p>
+				<?php } ?>
 				</div>
 				<?php
 				
